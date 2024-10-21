8a6b57a76434951e109367b86dff851413bbd7df
jakub karasek <jakubkarasek795@gmail.com>
2024-10-21 18:03:02 +0200

    added minor changes

--- Lab_01/Bazar.cpp
+++ Lab_01/Bazar.cpp
@@ -3,60 +3,52 @@
 
 using namespace std;
 
-
-void printArray(vector<long long> vec, int len){
-    for (int i = 0; i < len; i++){
-        cout << vec.at(i) << " ";
-    }
-    cout << endl;
-}
-
 int main() {
     long long n;
 
     cin >> n;
 
-    vector<long long> ceny(n,0);
-    vector<long long> NP(n, 0);
-    vector<long long> P(n, 0);
-    vector<long long> wynik(n, -1);
-    long long suma = 0;
+    vector<long long> prices(n,0);
+    vector<long long> odd(n, 0);
+    vector<long long> even(n, 0);
+    vector<long long> result(n, -1);
+    long long sum = 0;
     long long minP = 0;
     long long minNP = 0;
 
     for (int i = 1; i <= n; i++) {
         long long element;
         cin >> element;
-        ceny[i] = element;
-        wynik[i] = -1;
+        prices[i] = element;
+        result[i] = -1;
 
-        if (ceny[i] % 2 == 1) {
-            NP[i] = ceny[i];
-            P[i] = P[i - 1];
+        if (prices[i] % 2 == 1) {
+            odd[i] = prices[i];
+            even[i] = even[i - 1];
         } else {
-            P[i] = ceny[i];
-            NP[i] = NP[i - 1];
+            even[i] = prices[i];
+            odd[i] = odd[i - 1];
         }
     }
 
     for (int i = n; i; i--) {
-        suma += ceny[i];
+        sum += prices[i];
 
-        if (ceny[i] % 2 == 1) {
-            minNP = ceny[i];
+        if (prices[i] % 2 == 1) {
+            minNP = prices[i];
         } else {
-            minP = ceny[i];
+            minP = prices[i];
         }
 
-        if (suma % 2 == 1) { // mamy wynik
-            wynik[n + 1 - i] = suma;
+        if (sum % 2 == 1) {
+            result[n + 1 - i] = sum;
         } else {
-            if (P[i - 1] > 0LL && minNP > 0) {
-                wynik[n + 1 - i] = max(suma + P[i - 1] - minNP, wynik[n + 1 - i]);
+            if (even[i - 1] > 0LL && minNP > 0) {
+                result[n + 1 - i] = max(sum + even[i - 1] - minNP, result[n + 1 - i]);
             }
 
-            if (NP[i - 1] > 0LL && minP > 0) {
-                wynik[n + 1 - i] = max(suma + NP[i - 1] - minP, wynik[n + 1 - i]);
+            if (odd[i - 1] > 0LL && minP > 0) {
+                result[n + 1 - i] = max(sum + odd[i - 1] - minP, result[n + 1 - i]);
             }
         }
     }
@@ -66,7 +58,7 @@ int main() {
     for (int i = 0; i < n; i++){
         int k;
         cin >> k;
-        cout << wynik[k] << endl;
+        cout << result[k] << endl;
     }
 
     return 0;
--- Lab_01/Matryca.cpp
+++ Lab_01/Matryca.cpp
@@ -3,35 +3,35 @@
 using namespace std;
 
 int main() {
-    string tapeta;
+    string wallpaper;
     
-    cin >> tapeta;
+    cin >> wallpaper;
     
-    int akt = tapeta[0];
-    int lastIndeks = 0;
-    int wynik = tapeta.length();
+    int curr = wallpaper[0];
+    int lastIndex = 0;
+    int result = wallpaper.length();
 
-    for (int i = 0; i < tapeta.length(); i++){
-        if (tapeta[i] != '*'){
-            if (akt == '*'){
-                lastIndeks = i;
-                akt = tapeta[i];
+    for (int i = 0; i < wallpaper.length(); i++){
+        if (wallpaper[i] != '*'){
+            if (curr == '*'){
+                lastIndex = i;
+                curr = wallpaper[i];
             }
-            else if (akt == tapeta[i]){
-                lastIndeks = i;
+            else if (curr == wallpaper[i]){
+                lastIndex = i;
             } else {
-                wynik = min(wynik, i - lastIndeks - 1);
-                lastIndeks = i;
-                akt = tapeta[i];
+                result = min(result, i - lastIndex - 1);
+                lastIndex = i;
+                curr = wallpaper[i];
             }
         }
     }
 
-    if (wynik == tapeta.length()){
+    if (result == wallpaper.length()){
         cout << 1 << endl;
         return 0;
     }
 
-    cout << tapeta.length() - wynik << endl;
+    cout << wallpaper.length() - result << endl;
     return 0;
 }
